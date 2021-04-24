35428a69a2f57e0d7b850bde73dfbda52e728552
Pgturn68 <pgturn68@gmail.com>
2021-04-24 10:41:25 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,51 +1,75 @@
-// Moment.js
+// Moment
 var dayDate = moment().format("dddd, MMMM Do YYYY")
 var dayPlusOne = moment().add(1, "days").format("ddd, MMM D");
 var dayPlusTwo = moment().add(2, 'days').format('ddd, MMM D');
 var dayPlusThree = moment().add(3, 'days').format('ddd, MMM D');
 var dayPlusFour = moment().add(4, 'days').format('ddd, MMM D');
 var dayPlusFive = moment().add(5, 'days').format('ddd, MMM D');
+var dayPlusSix = moment().add(6, 'days').format('ddd, MMM D');
 
+
+
+// application variables
 var cities = JSON.parse(localStorage.getItem('cities')) || [];
 
-$("#cityName").keypress(function () {
-  var _val = $("#cityName").val();
-  var _txt = _val.charAt(0).toUpperCase() + _val.slice(1);
-  $("#cityName").val(_txt);
-})
-// Today Parameters
-function displayCityInfo(chosenCity) {
+// event listeners
+$("#cityName").keypress(handleKeyPress);
+$("#clearList").click(handleClearClick);
+$(document).on("click", ".city", handleCityClick);
+$("#searchBtn").on("click", handleSearchClick);
+$(document).ready(init);
+
+// main
+renderButtons();
+
+
+
+
+// Weather 
+function displayCityInfo(chooseAcity) {
   $("#today").empty();
-  var city = chosenCity
+  var city = chooseAcity
   var queryURL = "https://api.openweathermap.org/data/2.5/forecast?q="+ city +"&units=imperial&appid=f1c6d934c46c2d1b26eb5e6679298844";
     $.ajax({
       url: queryURL,
       method: "GET"
-    }).then(function(response) {
-      console.log(response)
-// 5-Day Parameters             
-function getFiveDayForecast (){  
+    }).then(response => gotCityInfo(response,city));
+}
+
+function gotCityInfo(response,city) {
+    console.log(response);
+	getSixDayForecast(response,city);
+}
+
+function getSixDayForecast (response,city){  
   var lat = response.city.coord.lat;
   var lon = response.city.coord.lon;
   var queryURL = "https://api.openweathermap.org/data/2.5/onecall?lat="+lat+"&lon="+lon+"&units=imperial&appid=f1c6d934c46c2d1b26eb5e6679298844";
     $.ajax({
       url: queryURL,
       method: "GET"
-    }).then(function(response) {
+    }).then(response => gotSixDayForecast(response,city));
+}
+
+function gotSixDayForecast(response,city) {
     var todayIconLink = "http://openweathermap.org/img/wn/"+ response.current.weather[0].icon +"@2x.png";
     var dayOneIconLink = "http://openweathermap.org/img/wn/"+ response.daily[0].weather[0].icon +".png";
     var dayTwoIconLink = "http://openweathermap.org/img/wn/"+ response.daily[1].weather[0].icon +".png";
     var dayThreeIconLink = "http://openweathermap.org/img/wn/"+ response.daily[2].weather[0].icon +".png";
     var dayFourIconLink = "http://openweathermap.org/img/wn/"+ response.daily[3].weather[0].icon +".png";
     var dayFiveIconLink = "http://openweathermap.org/img/wn/"+ response.daily[4].weather[0].icon +".png";
+    var daySixIconLink = "http://openweathermap.org/img/wn/"+ response.daily[4].weather[0].icon +".png";
+
+
+
 
     console.log(response)
-    var display= $("<div class='searched-weather pl-4 py-3'>");
-    var displayCityDate = $("<h2>").text(city + ": "+ dayDate);
+    var display= $("<div class='searched-weather pl-3 py-3'>");
+    var displayCityDate = $("<h3>").text(city + ": "+ dayDate);
     display.append(displayCityDate);
 
-    // Today Information / Styling
-    var icon = $("<img class= 'float-right bg-info rounded mr-5' src="+todayIconLink+">");
+    // Forcast for Current day
+    var icon = $("<img class= 'center bg-info rounded mr-3' src="+todayIconLink+">");
       display.append(icon);
     var temp = $("<h4>").text(" Temperature: " + response.current.temp +"°F");
       display.append(temp);
@@ -58,7 +82,7 @@ function getFiveDayForecast (){
       display.append(uvi, uviVal);
     $("#today").append(display);
 
-    // U.V. Index Color
+    // U.V. Colors
     if (response.current.uvi<=2){
       $("#uvi").addClass("bg-success");
     } else if (response.current.uvi<=7){
@@ -67,52 +91,57 @@ function getFiveDayForecast (){
       $("#uvi").addClass("bg-danger");
     }
 
-    // 5-Day Information / Styling 
+    // Forcast for the next 5 days 
     $("#forecast").empty();
-    var firstDay= $("<div class='forecast-container col-md-2.4 bg-light rounded px-4 mt-4 py-2 ml-3'id='day-1'>");
+    
+    var firstDay= $("<div class='forecast-container col-md-2 bg-info rounded Regular shadow px-4 mt-4 py-2 ml-3'id='day-1'>");
     var dayOneDay = $("<h6>").text(dayPlusOne);
     var dayOneIcon = $("<img src="+dayOneIconLink+">");
     var dayOneTemp= $("<p>").text(" Temp: " +response.daily[0].temp.day+"°F");
     var dayOneHumid= $("<p>").text("Humid: "+response.daily[0].humidity+ "%");
     $(firstDay).append(dayOneDay, dayOneIcon, dayOneTemp, dayOneHumid);
-
-    var secondDay= $("<div class='forecast-container col-md-2.4 bg-light rounded px-4 mt-4 py-2 ml-4'id='day-2'>");
+    
+    var secondDay= $("<div class='forecast-container col-md-2 bg-info rounded Regular shadow px-4 mt-4 py-2 ml-4'id='day-2'>");
     var dayTwoDay = $("<h6>").text(dayPlusTwo);
     var dayTwoIcon = $("<img src="+dayTwoIconLink+">");
     var dayTwoTemp= $("<p>").text(" Temp: " +response.daily[1].temp.day+"°F");
     var dayTwoHumid= $("<p>").text("Humid: "+response.daily[1].humidity+ "%");
     $(secondDay).append(dayTwoDay, dayTwoIcon, dayTwoTemp, dayTwoHumid);
 
-    var thirdDay= $("<div class='forecast-container col-md-2.4 bg-light rounded px-4 mt-4 py-2 ml-4'id='day-3'>");
+    var thirdDay= $("<div class='forecast-container col-md-2 bg-info rounded Regular shadow px-4 mt-4 py-2 ml-4'id='day-3'>");
     var dayThreeDay = $("<h6>").text(dayPlusThree);
     var dayThreeIcon = $("<img src="+dayThreeIconLink+">");
     var dayThreeTemp= $("<p>").text(" Temp: " +response.daily[2].temp.day+"°F");
     var dayThreeHumid= $("<p>").text("Humid: "+response.daily[2].humidity+ "%");
     $(thirdDay).append(dayThreeDay, dayThreeIcon, dayThreeTemp, dayThreeHumid);
 
-    var fourthDay= $("<div class='forecast-container col-md-2.4 bg-light rounded px-4 mt-4 py-2 ml-4'id='day-4'>");
+    var fourthDay= $("<div class='forecast-container col-md-2 bg-info rounded Regular shadow px-4 mt-4 py-2 ml-4'id='day-4'>");
     var dayFourDay = $("<h6>").text(dayPlusFour);
     var dayFourIcon = $("<img src="+dayFourIconLink+">");
     var dayFourTemp= $("<p>").text(" Temp: " +response.daily[3].temp.day+"°F");
     var dayFourHumid= $("<p>").text("Humid: "+response.daily[3].humidity+ "%");
     $(fourthDay).append(dayFourDay, dayFourIcon, dayFourTemp, dayFourHumid);
 
-    var fifthDay= $("<div class='forecast-container col-md-2.4 bg-light rounded px-4 mt-4 py-2 ml-4'id='day-5'>");
+    var fifthDay= $("<div class='forecast-container col-md-2 bg-info rounded Regular shadow px-4 mt-4 py-2 ml-4'id='day-5'>");
     var dayFiveDay = $("<h6>").text(dayPlusFive);
     var dayFiveIcon = $("<img src="+dayFiveIconLink+">");
     var dayFiveTemp= $("<p>").text(" Temp: " +response.daily[4].temp.day+"°F");
     var dayFiveHumid= $("<p>").text("Humid: "+response.daily[4].humidity+ "%");
     $(fifthDay).append(dayFiveDay, dayFiveIcon, dayFiveTemp, dayFiveHumid);
 
-    $("#forecast").append(firstDay, secondDay, thirdDay, fourthDay, fifthDay);
-  });
-};
+    var sixthDay= $("<div class='forecast-container col-md-2 bg-info rounded Regular shadow px-4 mt-4 py-2 ml-4'id='day-5'>");
+    var daySixDay = $("<h6>").text(dayPlusSix);
+    var daySixIcon = $("<img src="+daySixIconLink+">");
+    var daySixTemp= $("<p>").text(" Temp: " +response.daily[5].temp.day+"°F");
+    var daySixHumid= $("<p>").text("Humid: "+response.daily[5].humidity+ "%");
+    $(sixthDay).append(daySixDay, daySixIcon, daySixTemp, daySixHumid);
 
-getFiveDayForecast();
-          })
-        }
+    $("#forecast").append(firstDay, secondDay, thirdDay, fourthDay, fifthDay, sixthDay);
+ }
 
-=> renderButtons() {
+
+//event handlers
+function renderButtons() {
   $("#history").empty();
   for (var i = 0; i < cities.length; i++) {
     var a = $("<button class= 'btn-outline-info mb-1 mt-0 btn d-flex justify-content-center btn-default btn-block'>");
@@ -120,17 +149,21 @@ getFiveDayForecast();
     a.attr("chosenCity", cities[i]);
     a.text(cities[i]);
     $("#history").prepend(a);
-    }
   }
+}
 
-renderButtons();
-
-$(document).ready(function() {
+function init() {
   var chosenCity = cities.pop();
   displayCityInfo(chosenCity);
-})
+}
 
-$("#searchBtn").on("click", function(event) {
+function handleKeyPress() {
+  var _val = $("#cityName").val();
+  var _txt = _val.charAt(0).toUpperCase() + _val.slice(1);
+  $("#cityName").val(_txt);
+}
+
+function handleSearchClick(event) {
     event.preventDefault();
     var chosenCity = $("#cityName").val().trim();
   if (cities.indexOf(chosenCity)===-1){
@@ -139,16 +172,16 @@ $("#searchBtn").on("click", function(event) {
     renderButtons();
   }
   displayCityInfo(chosenCity);
-});
+}
 
-$(document).on("click", ".city", function(event) {
+function handleCityClick(event) {
   event.preventDefault();
   var chosenCity = $(this).text();
   displayCityInfo(chosenCity); 
-})
+}
 
-$("#clearList").click(function(event) {
+function handleClearClick(event) {
   event.preventDefault();
   $("#history").empty();
   localStorage.clear(); 
-})
+}
